4d4c26e4cce1930307a856d20e438f1ac4e5e665
Adrian Cotfas <adrian.cotfas@ingka.com>
2023-10-04 10:34:55 +0000

    01_05 done

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,20 @@
-// Write your code here
+// Write your code hereclass 
+
+class User {
+  #userName;
+  #pasword;
+  constructor(userName, pasword, age) {
+    this.#userName = userName;
+    this.#pasword = pasword;
+    this.age = age;
+  }
+  getUser() {
+    return  this.#user() ;
+  }
+  #user() {
+    return `${this.#userName}, ${this.#pasword}, ${this.age}`;
+  }
+}
+
+let user = new User('John', 'Xe983kj', 45);
+console.log(user.userName, user.pasword, user.age, user.getUser());
