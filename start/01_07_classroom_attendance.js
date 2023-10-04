4d33bc972d4b091425cd07ccfad0b9997f69d5e4
Adrian Cotfas <adrian.cotfas@ingka.com>
2023-10-04 11:32:32 +0000

    01_07 Done

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,18 @@
 // Write your code here
+
+const getStudents = (classroom) => {
+  let { hasTeachingAssistant, classlist} = classroom;
+  let teacher, theachingAssistant, students;
+
+  if(hasTeachingAssistant) {
+    [teacher,theachingAssistant, ...students] = classlist;
+  } else {
+    [teacher,...students] = classlist;
+  }
+  return students
+}
+
+
+const classroom = {hasTeachingAssistant: true, classlist: ["Teacher","TeachingAssistant","Student 1","Studen 2"] };
+
+console.log ("Sturdents : ", getStudents(classroom));
