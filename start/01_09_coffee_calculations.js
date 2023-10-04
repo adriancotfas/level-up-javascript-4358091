14cf631cff5b65ff06f081cd34b6536f30c04530
Adrian Cotfas <adrian.cotfas@ingka.com>
2023-10-04 12:30:39 +0000

    01_09 Done

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,6 @@
 // Write your code here
+const orders = [2, 5, 7, 1, 4];
+const sumOrders =  orders.reduce((accumulator, currentValue) => 
+accumulator + (currentValue*1.25), 0);
+
+console.log(`The total bill is ${sumOrders}`);
