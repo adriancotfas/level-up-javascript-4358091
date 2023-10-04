5dca4cb13344b1995d9305689774e92695f59be6
Adrian Cotfas <adrian.cotfas@ingka.com>
2023-10-04 09:00:45 +0000

    01_02 done

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,18 @@
 // Write your code here
+
+class Movie  {  
+  constructor(Title, Director, Gene, ReleaseYear, Rating) {
+    this.title = Title;
+    this.director = Director;
+    this.gene = Gene;
+    this.releaseYear = ReleaseYear;
+    this.rating = Rating;
+  }
+  // Getter
+  get Overview() {
+    return `${this.title}, a ${this.gene} film directed by ${this.director} was relesed in ${this.releaseYear}, it receved a rating of ${this.rating}`;
+  }
+}
+
+const movie = new Movie("Movie Title", "Movie director", "Movie gene", "Movie releaseYear","Movie rating");
+console.log( movie.Overview ); 
