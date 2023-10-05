46afe38c648ab1a6930d7e0a9317fbb10c414b26
Adrian Cotfas <adrian.cotfas@ingka.com>
2023-10-05 11:06:07 +0000

    01_15 Done

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,9 @@
 // Write your code here
+
+function validPassword(string) {
+  let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g;
+  const valid = string.search(regex);
+  console.log(valid)
+  return valid ? "Invalid" : "Valid";
+}
+console.log(validPassword("Mysqwd4!"));
