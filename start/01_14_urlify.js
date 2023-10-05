cd5c30dbd1457161f4c7e1c7440b6a725a2cc5b3
Adrian Cotfas <adrian.cotfas@ingka.com>
2023-10-05 10:13:58 +0000

    01_14 Done

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,6 @@
 // Write your code here
+function URLFrendly(string) {
+  return string.trim().replace(/[^0-9a-z ]/gi, '').replace(/[ ]/gi, '-').toLowerCase();
+}
+console.log(URLFrendly(" My blog name!"));
+console.log(URLFrendly("Emma's blog"));
